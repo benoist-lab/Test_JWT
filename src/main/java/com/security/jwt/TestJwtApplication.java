5fa1a3da0ada0b243b3a01d30cc8729905d732ba
Dubuc Benoist <dubucbenoist@yahoo.fr>
2021-09-19 18:37:49 +0200

    first commit

---
+++ src/main/java/com/security/jwt/TestJwtApplication.java
@@ -0,0 +1,44 @@
+package com.security.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+/* exemple issue de
+ * https://www.javainuse.com/spring/boot-jwt
+ * 
+ * 
+ autre recherche
+https://www.developpez.net/forums/d1729986/javascript/general-javascript/crypter-mots-passe-front-end-back-end/
+
+https://www.javainuse.com/spring/boot-jwt
+
+https://www.digitalocean.com/community/tutorials/vuejs-vue-jwt-patterns  ???
+
+
+exemple avec un token
+https://stackoverflow.com/questions/67657900/jwt-token-does-not-begin-with-bearer-string-and-in-axioms-when-sending-it-began
+
+
+https://www.bezkoder.com/spring-boot-vue-js-authentication-jwt-spring-security/
+https://www.bezkoder.com/vue-3-authentication-jwt/
+
+https://www.npmjs.com/package/axios-jwt
+
+
+https://www.techiediaries.com/vue-vuex-axios-auth/
+ */
+
+
+
+
+@SpringBootApplication
+public class TestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJwtApplication.class, args);
+	}
+
+}
